faf655633fbd43aee96998f4030412c3564ff86c
Daniel Bostock <danielbostock@outlook.com>
2021-05-22 22:55:56 +1000

    v0.2a added selection functionality

--- bronzium_opener/bronzium_opener.py
+++ bronzium_opener/bronzium_opener.py
@@ -1,15 +1,50 @@
+#Start Import Area
+from os import system, name 
+from time import sleep 
 import time, pyautogui
+from bronzium_opener import text
+import bronzium_opener
+#End Import Area
+  
+# define our clear function 
+def clear(): 
+  
+    # Windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # Unix Distros (Including MacOS)
+    else: 
+        _ = system('clear') 
 
-def opener(timer):
 
-    print('Starting The Bronzium Pack Opener in 5 Seconds...')
-    time.sleep(5)
-    print('\nPack opening has now begun and will run for ' + str(timer) + ' seconds')
-
-    while timer > 0:
-        pyautogui.press('1')
-        time.sleep(1.5)
-        timer -= 1.5
+def opener():
+    print(text.INTRO)
+    input('\nPress ENTER to continue...')
+    clear()
+    print(text.SELECTION)
+    selection = int(input('Selection: '))
+    clear()
+    if selection == 1:
+        cycle = 210
+        cycles = 0
+        while cycles == 0:
+            cycles = int(input('Specify Cycles: '))
+            cycle *= cycles
+            timer = cycle
+            timer_mins = timer
+            timer_mins /= 60
+            print('\nCurrent specified run time for the opener\nCycles: ' + str(cycles) + '\nTime: ' + str(timer) + ' seconds')
+            confirm = int(input('\n\n1.Start\n2.Adjust run time for the opener\n3.Abort\n\nSelection: '))
+            if confirm == 1:
+                print(text.STARTING)
+                print('\n\nThis pack opener will be running for ' + str(timer_mins) + ' minutes...' )
+                time.sleep(5)
+                
+                while timer > 0:
+                    pyautogui.press('1')
+                    time.sleep(1.5)
+                    timer -= 1.5
 
 
 
